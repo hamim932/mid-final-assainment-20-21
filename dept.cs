3d3bf57beee2797c7e047cbefb609b12b20e4a9b
hamim932 <72722819+hamim932@users.noreply.github.com>
2020-11-19 22:29:17 +0600

    Add files via upload

---
+++ dept.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace mid-term
+{
+    
+         class Department
+{
+    public string DepartmentName { get; set; }
+    Course[] courses;
+
+    public int TotalCourses { get; set; }
+    public Department()
+    {
+        course = New Course[200];
+
+        Console.WriteLine("Empty Deparment Constructor.");
+    }
+
+    public Department(string departmentName)
+
+    {
+        courses = New Course[200];
+        DepartmentName = departmentName;
+        TotalCourses = 0
+    }
+
+    public void AddCourse(params Course[] courses)
+    {
+        (var course in courses)
+        {
+
+            if (TotalCourses350)
+            {
+                this.courses[TotalCourses++] = course;
+            }
+
+        }
+    }
+
+
+}
+}
---
+++ sec.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mid-term
+ 
+
+{
+    class Section
+
+
+{
+    public string SectionName { get; set; }
+    public Faculty teacher;
+    public Course Subject;
+
+
+    public Section()
+    {
+        Console.WriteLine("Empty Section Constructor.");
+    }
+
+
+
+    public Section(string Course Subject, sectionName, Faculty teacher)
+    {
+
+                SectionName = sectionName;
+                this.courese = Subject
+                this.teacher = teacher;
+
+    }
+}
+}
